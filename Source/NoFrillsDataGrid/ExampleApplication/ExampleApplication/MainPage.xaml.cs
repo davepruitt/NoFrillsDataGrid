2aea433a213b164e3aca9b0c523a8fb92aa5573a
David Pruitt <davepruitt@users.noreply.github.com>
2019-06-24 18:02:30 -0500

    Updating location of example project

--- Source/NoFrillsDataGrid/ExampleApplication/ExampleApplication/MainPage.xaml.cs
+++ Source/NoFrillsDataGrid/ExampleApplication/ExampleApplication/MainPage.xaml.cs
@@ -11,6 +11,8 @@ using Xamarin.Forms;
 
 namespace ExampleApplication
 {
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
