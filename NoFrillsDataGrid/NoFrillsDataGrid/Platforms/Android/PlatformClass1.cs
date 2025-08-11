b4512441549b39bbf022f38ef087127e3ff2c790
David <15479248+davepruitt@users.noreply.github.com>
2025-08-11 11:09:13 -0600

    Moved old version to a separate folder temporarily. Began work on new version. Updated code in the NoFrillsDataGrid class so that we don't use deprecated/obsolete functions/properties in SkiaSharp.

---
+++ NoFrillsDataGrid/NoFrillsDataGrid/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace NoFrillsDataGrid
+{
+    // All the code in this file is only included on Android.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ NoFrillsDataGrid/NoFrillsDataGrid/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NoFrillsDataGrid
+{
+	// All the code in this file is only included on Tizen.
+	public class PlatformClass1
+	{
+	}
+}
