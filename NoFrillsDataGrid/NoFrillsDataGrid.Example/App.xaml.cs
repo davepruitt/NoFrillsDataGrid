197ff866849210370ef878384414ea71d36731a2
David <15479248+davepruitt@users.noreply.github.com>
2025-08-21 19:32:32 -0600

    Created a project that I'll use for an example Maui app that uses the data grid.

---
+++ NoFrillsDataGrid/NoFrillsDataGrid.Example/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace NoFrillsDataGrid.Example
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ NoFrillsDataGrid/NoFrillsDataGrid.Example/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NoFrillsDataGrid.Example
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
